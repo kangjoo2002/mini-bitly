71a272dbddcc2735850025693502865229953c48
joohyeongzz81 <joohyeongzz81@gmail.com>
2026-05-10 16:52:47 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/example/minibitly/MiniBitlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.minibitly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBitlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBitlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minibitly/MiniBitlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minibitly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBitlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
